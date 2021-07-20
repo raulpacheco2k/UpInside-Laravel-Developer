f304712ebb76915c0e07ae8e78fd35c270c80dbb
Raul Pacheco <eu@raulpacheco.com.br>
2021-07-19 23:40:45 -0300

    Adiciona MaritalStatus

---
+++ app/Models/MaritalStatus.php
@@ -0,0 +1,40 @@
+<?php
+
+
+namespace App\Models;
+
+
+class MaritalStatus
+{
+    public const NOT_MARRIED = 1;
+    public const MARRIED = 2;
+    public const SEPARATE = 3;
+    public const DIVORCED = 4;
+    public const WIDOWER = 5;
+
+    public const TYPES = self::NOT_MARRIED . "," . self::MARRIED . "," . self::SEPARATE . "," . self::DIVORCED . "," . self::WIDOWER;
+
+    public static function labels(): array
+    {
+        return array(
+            'Spouse Not Required' => array(
+                self::NOT_MARRIED => 'Not married',
+                self::SEPARATE => 'Separate',
+                self::DIVORCED => 'Divorced',
+                self::WIDOWER => 'Widower'
+            ),
+            'Required Spouse' => array(
+                self::MARRIED => 'Married',
+
+            )
+        );
+    }
+
+    /**
+     * @return array
+     */
+    final public static function toArray(): array
+    {
+        return self::labels();
+    }
+}
